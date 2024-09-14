d5f696ba9b189c3e9235badf32e9a1658125eff6
beppuayakab51 <pz8xmgdv39@gmail.com>
2024-09-14 12:40:16 +0900

    翻訳文フォーム追加とshow

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create]
   root "static_pages#top"
-  resources :books, only: %i[index edit new create]
-  resources :words, only: %i[index edit new create]
+  resources :books, only: %i[index edit new create show]
+  resources :words, only: %i[index edit new create show]
   # ログイン関係
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
